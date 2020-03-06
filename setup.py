69db6f69fce86a02ccb3535cd794cfe728a3f074
Jaime Pineda <jaime.e.pineda@gmail.com>
2020-03-06 12:14:50 +0100

    cleanup of setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import glob
 import os
-# import sys
-
-# import ah_bootstrap
 from setuptools import setup
 
 PACKAGENAME = "velocity_tools"
-DESCRIPTION = 'Tools to help analysis of velocity cubes/field'
+DESCRIPTION = 'Tools to help analysis of velocity cubes and velocity fields'
 AUTHOR = 'Jaime E. Pineda'
 AUTHOR_EMAIL = 'jpineda@mpe.mpg.de'
 LICENSE = "MIT"
 URL = "https://github.com/jpinedaf/velocity_tools.git"
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.4.dev1'
+VERSION = '0.4.dev2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 def read(fname):
@@ -34,11 +30,3 @@ setup(
     license=LICENSE,
     url=URL,
     )
-      # scripts=scripts,
-      # long_description=LONG_DESCRIPTION,
-      # cmdclass=cmdclassd,
-      # zip_safe=False,
-      # use_2to3=True,
-      # entry_points=entry_points,
-      # **package_info
-# )
