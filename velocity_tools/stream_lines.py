Corrected tolerance units

velocity_tools/stream_lines.py
@@ -96,7 +96,8 @@ def stream_line(r, mass=0.5 * u.Msun, r0=1e4 * u.au, theta0=30 * u.deg,
     # the first element in the streamline is the starting point
     theta[0] = rad_theta0
     # Initial guess at largest radius is theta0 +- initguess towards the midplane
-    deltar = np.amin(np.abs(np.roll(r,1) - r)) * u.au
+    deltar = np.amin(np.abs(np.roll(r,1) - r))
+    print(deltar)
     # we use a constant of 6e-5 for an epsilon of 0.01 km/s
     # this result will be in radians
     tol = (6.e-5 * deltar * omega / (v_r0+ 0.1 * u.km/u.s)).decompose().value